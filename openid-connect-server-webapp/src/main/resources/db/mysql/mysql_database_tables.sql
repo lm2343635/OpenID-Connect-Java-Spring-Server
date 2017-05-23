6f1d4508675c493632c9950fc914eb207f1c1a32
limeng <lm2343635@126.com>
2017-05-23 12:56:16 +0900

    Fix mysql related issues.

--- openid-connect-server-webapp/src/main/resources/db/mysql/mysql_database_tables.sql
+++ openid-connect-server-webapp/src/main/resources/db/mysql/mysql_database_tables.sql
@@ -133,48 +133,48 @@ CREATE TABLE IF NOT EXISTS client_details (
 	device_code_validity_seconds BIGINT,
 	
 	client_id VARCHAR(256),
-	client_secret VARCHAR(2048),
+	client_secret VARCHAR(1024),
 	access_token_validity_seconds BIGINT,
 	refresh_token_validity_seconds BIGINT,
-	
+
 	application_type VARCHAR(256),
 	client_name VARCHAR(256),
 	token_endpoint_auth_method VARCHAR(256),
 	subject_type VARCHAR(256),
-	
-	logo_uri VARCHAR(2048),
-	policy_uri VARCHAR(2048),
-	client_uri VARCHAR(2048),
-	tos_uri VARCHAR(2048),
 
-	jwks_uri VARCHAR(2048),
-	jwks VARCHAR(8192),
-	sector_identifier_uri VARCHAR(2048),
-	
+	logo_uri VARCHAR(1024),
+	policy_uri VARCHAR(1024),
+	client_uri VARCHAR(1024),
+	tos_uri VARCHAR(1024),
+
+	jwks_uri VARCHAR(1024),
+	jwks VARCHAR(1024),
+	sector_identifier_uri VARCHAR(1024),
+
 	request_object_signing_alg VARCHAR(256),
-	
+
 	user_info_signed_response_alg VARCHAR(256),
 	user_info_encrypted_response_alg VARCHAR(256),
 	user_info_encrypted_response_enc VARCHAR(256),
-	
+
 	id_token_signed_response_alg VARCHAR(256),
 	id_token_encrypted_response_alg VARCHAR(256),
 	id_token_encrypted_response_enc VARCHAR(256),
-	
+
 	token_endpoint_auth_signing_alg VARCHAR(256),
-	
+
 	default_max_age BIGINT,
 	require_auth_time BOOLEAN,
 	created_at TIMESTAMP NULL,
-	initiate_login_uri VARCHAR(2048),
+	initiate_login_uri VARCHAR(1024),
 	clear_access_tokens_on_refresh BOOLEAN DEFAULT true NOT NULL,
-	
-	software_statement VARCHAR(4096),
-	software_id VARCHAR(2048),
-	software_version VARCHAR(2048),
-	
+
+	software_statement VARCHAR(1024),
+	software_id VARCHAR(1024),
+	software_version VARCHAR(1024),
+
 	code_challenge_method VARCHAR(256),
-	
+
 	UNIQUE (client_id)
 );
 
